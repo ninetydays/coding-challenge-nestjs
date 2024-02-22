7a05075d2ca572149e9e97707b39505387e786ca
JonghunYu <yuzong@naver.com>
2024-02-22 14:48:18 +0900

    feat: added DatabaseModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from 'libs/database';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: [`.env.${process.env.NODE_ENV}`, '.env.local'],
     }),
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
